678b1bd7c4e6bbd37898b480a860e4d23622f032
damartinezru <danielamr15@hotmail.com>
2018-09-02 17:12:58 -0500

    patron prototipo

---
+++ Prototipo/src/Objects/Folder.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+import Prototipos.*;
+import Duplicar.Duplicar;
+
+
+/**
+ *
+ * @author GOLDS
+ */
+public class Folder {
+    public HojaPrototipo hoja;
+    public SeparadorPrototipo separador;
+
+    public HojaPrototipo getHoja() {
+        return hoja;
+    }
+
+    public void setHoja(HojaPrototipo hoja) {
+        this.hoja = hoja;
+    }
+
+    public SeparadorPrototipo getSeparador() {
+        return separador;
+    }
+
+    public void setSeparador(SeparadorPrototipo separador) {
+        this.separador = separador;
+    }
+    
+    public double getAltoHoja(){
+        double alto = getHoja().getAlto();
+        return alto;
+    }
+    public void setAltoHoja(int alto){
+        getHoja().setAlto(alto);
+    }
+    public double getAnchoHoja(){
+        double alto = getHoja().getAncho();
+        return alto;
+    }
+    public void setAnchoHoja(int alto){
+        getHoja().setAncho(alto);
+    }
+    public double getAltoSeparador(){
+        double altoS = getSeparador().getAltoS();
+        return altoS;
+    }
+    public void setAltoSeparador(int altoS){
+        getSeparador().setAltoS(altoS);
+    }
+    public double getAnchoSeparador(){
+        double anchoS = getSeparador().getAnchoS();
+        return anchoS;
+    }
+    public void setAnchoSeparador(int anchoS){
+        getSeparador().setAnchoS(anchoS);
+    }
+}
---
+++ Prototipo/src/Prototipo/PruebaPrototipo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prototipo;
+import Objects.*;
+import Prototipos.*;
+import Duplicar.Duplicar;
+
+
+/**
+ *
+ * @author GOLDS
+ */
+public class PruebaPrototipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        FolderPrototipo folderBase = new FolderPrototipo();
+        folderBase.setHoja(new HojaPrototipo(30,10));
+        folderBase.setSeparador(new SeparadorPrototipo(31,12));
+        
+        FolderPrototipo folderDupA = new FolderPrototipo();
+        folderDupA = (FolderPrototipo)folderBase.duplicar();
+        
+        FolderPrototipo folderDupB = new FolderPrototipo();
+        folderDupB = (FolderPrototipo)folderBase.duplicar();
+        
+        System.out.println("info base: "+folderBase);
+        System.out.println("info duplicado 1:"+folderDupA);
+        // TODO code application logic here
+    }
+    
+}
