98ded38af02f082a243246fb133f911d0d24e033
Jorge Andrés Rojas Bautista <36830829+jorgearojas25@users.noreply.github.com>
2019-01-27 18:04:00 -0500

    Patron

---
+++ PruebaPatronFlyweight/src/pruebapatronflyweight/PruebaPatronFlyweight.java
@@ -0,0 +1,22 @@
+/*
+ * Proyecto para clase de Modelos de Programación I
+ * Universidad Distrital Francisco Jose de Caldas
+ * Profesor Julio Baron    
+ */
+package pruebapatronflyweight;
+
+/**
+ *
+ * @author Jorge Andres
+ */
+public class PruebaPatronFlyweight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+ 
+    }
+    
+}
